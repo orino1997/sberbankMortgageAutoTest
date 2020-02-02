91e2867502db44d17449b0529971e6aa77d89375
orino1997 <40629102+orino1997@users.noreply.github.com>
2020-02-02 18:29:04 +0300

    try to add allure screenshots 2

--- src/main/java/sberbankMortgage/AllureReporter.java
+++ src/main/java/sberbankMortgage/AllureReporter.java
@@ -3,6 +3,12 @@ package sberbankMortgage;
 import cucumber.api.event.EventPublisher;
 import cucumber.api.event.TestCaseFinished;
 import cucumber.api.formatter.Formatter;
+import io.qameta.allure.Allure;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 
 public class AllureReporter implements Formatter {
@@ -12,8 +18,15 @@ public class AllureReporter implements Formatter {
         eventPublisher.registerHandlerFor(TestCaseFinished.class, this::handleTestFinished);
     }
     private void handleTestFinished(TestCaseFinished event) {
+        System.out.println(1);
         if(!event.result.isOk(true)) {
-               }
+            System.out.println(2);
+            System.out.println(BaseTest.getDriver());
+            byte[] screenShot = ((TakesScreenshot) BaseTest.getDriver()).getScreenshotAs(OutputType.BYTES);
+            System.out.println(screenShot.length);
+            Allure.getLifecycle().addAttachment(LocalDateTime.now()
+                    .format(DateTimeFormatter.ofPattern("dd-MMM-yy_hh:mm:ss")), "image/png", "png", screenShot);}
+
     }
 }
 
