0a4733cf7fc52f2500c6d72787e9d61d05aa1027
orino1997 <40629102+orino1997@users.noreply.github.com>
2020-02-05 22:46:18 +0300

    remotedriver support

--- src/main/java/sberbankMortgage/BaseTest.java
+++ src/main/java/sberbankMortgage/BaseTest.java
@@ -7,7 +7,11 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Properties;
@@ -17,31 +21,40 @@ public class BaseTest {
     protected static WebDriver driver;
     protected static String URL;
     public static Properties properties = TestProperties.getInstance().getProperties();
-    public static WebDriver getDriver() {
-        return driver;
-    }
 
-    public void initialize() {
+    public void initialize() throws MalformedURLException {
         String browser = properties.getProperty("browser");
-        if ("chrome".equals(browser)) {
-            System.setProperty("webdriver.chrome.driver", properties.getProperty("webdriver.chrome.driver"));
-            driver = new ChromeDriver();
-        } else if ("firefox".equals(browser)) {
-            System.setProperty("webdriver.gecko.driver", properties.getProperty("webdriver.gecko.driver"));
-            driver = new ChromeDriver();
-        } else if ("IE".equals(browser)) {
-            System.setProperty("webdriver.ie.driver", properties.getProperty("webdriver.ie.driver"));
-            driver = new InternetExplorerDriver();
-
-            System.setProperty("webdriver.chrome.driver", properties.getProperty("webdriver.chrome.driver"));
-            driver = new ChromeDriver();
+        String isRemote = properties.getProperty("isRemote");
+        if (isRemote.equals("true")) {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            capabilities.setVersion("73.0");
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", false);
+            capabilities.setCapability("enableLog", false);
+            driver = new RemoteWebDriver(URI.create("http://selenoid.aplana.com:4445/wd/hub/").toURL(),
+                    capabilities);
         } else {
-            System.setProperty("webdriver.chrome.driver", properties.getProperty("webdriver.chrome.driver"));
-            driver = new ChromeDriver();
+            if ("chrome".equals(browser)) {
+                System.setProperty("webdriver.chrome.driver", properties.getProperty("webdriver.chrome.driver"));
+                driver = new ChromeDriver();
+            } else if ("firefox".equals(browser)) {
+                System.setProperty("webdriver.gecko.driver", properties.getProperty("webdriver.gecko.driver"));
+                driver = new ChromeDriver();
+            } else if ("IE".equals(browser)) {
+                System.setProperty("webdriver.ie.driver", properties.getProperty("webdriver.ie.driver"));
+                driver = new InternetExplorerDriver();
+
+                System.setProperty("webdriver.chrome.driver", properties.getProperty("webdriver.chrome.driver"));
+                driver = new ChromeDriver();
+            } else {
+                System.setProperty("webdriver.chrome.driver", properties.getProperty("webdriver.chrome.driver"));
+                driver = new ChromeDriver();
+            }
+            URL = properties.getProperty("appURL");
+            driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+            driver.manage().window().maximize();
         }
-        URL = properties.getProperty("appURL");
-        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
     }
 
     public void tearDown(Scenario scenario) {
